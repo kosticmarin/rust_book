372b73be2ad9b22cc117efd3ed7e52f637117087
Marin Koštić <kosticmarin@gmail.com>
2019-02-05 08:58:08 +0100

    Add static impl for User

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -8,6 +8,17 @@ struct User {
     active: bool,
 }
 
+impl User {
+    // static fn
+    fn build_user(email: String, username: String) -> User {
+        User {
+            username,
+            email,
+            sign_in_count: 0,
+            active: false,
+        }
+    }
+}
 fn main() {
     let user1 = User {
         username: String::from("johanez"),
@@ -16,4 +27,18 @@ fn main() {
         sign_in_count: 1,
     };
     dbg!(user1);
+
+    let mut user2 = User {
+        username: String::from("user_name"),
+        email: String::from("username@example.com"),
+        active: true,
+        sign_in_count: 2,
+    };
+    dbg!(&user2);
+    user2.active = false;
+    dbg!(&user2);
+    dbg!(User::build_user(
+        String::from("email@example.com"),
+        String::from("user")
+    ));
 }
