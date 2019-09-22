ae818c8754b027e21ddd098d62bc1c009b32a7f9
Marin Koštić <kosticmarin@gmail.com>
2019-09-22 11:47:29 +0200

    Implement solution-01
    - Change conditions for factorial
    - Update tests
    - Implement main()

--- lectures/lecture-01/solution-01/src/factorial.rs
+++ lectures/lecture-01/solution-01/src/factorial.rs
@@ -1,5 +1,7 @@
+use std::io::{self, Write};
+
 fn factorial(n: i32) -> Result<i64, String> {
-    if n < 2 || n > 20 {
+    if n < 0 || n > 20 {
         Err(format!(
             "'{}' is not in the valid range. Try a number from range [2, 20]",
             n
@@ -14,7 +16,31 @@ fn factorial(n: i32) -> Result<i64, String> {
 }
 
 fn main() {
-    println!("Hello, factorial");
+    let input = io::stdin();
+    let mut output = io::stdout();
+    loop {
+        let mut line = String::new();
+        print!("Enter a number >");
+        output.flush().expect("Error flushing stdout!");
+        input
+            .read_line(&mut line)
+            .expect("Error reading from stdin!");
+        let trimed_line = line.trim().to_lowercase();
+        if &trimed_line == "exit" {
+            break;
+        }
+        let n: i32 = match trimed_line.parse() {
+            Ok(v) => v,
+            Err(_) => {
+                println!("'{}' is not a valid number.", trimed_line);
+                continue;
+            }
+        };
+        match factorial(n) {
+            Ok(v) => println!("{}! = {}", n, v),
+            Err(e) => println!("{}", e),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -22,12 +48,16 @@ mod tests {
     use super::*;
     #[test]
     fn zero_fact_is_err() {
-        assert!(factorial(0).is_err());
+        let result = factorial(0);
+        assert!(result.is_ok());
+        assert_eq!(1i64, result.unwrap());
     }
 
     #[test]
     fn one_fact_is_err() {
-        assert!(factorial(1).is_err());
+        let result = factorial(1);
+        assert!(result.is_ok());
+        assert_eq!(1i64, result.unwrap());
     }
 
     #[test]
